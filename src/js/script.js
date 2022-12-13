ede49c2cf068a96d97956469c01d13cb56485b45
Natalie <natasha.pogorenko@gmail.com>
2022-12-13 19:45:38 +0200

    project assembled

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import calc from './modules/calc';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import timer from './modules/timer';
+import { modalOpen } from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    
+    const modalTimerId = setTimeout(()=>modalOpen('.modal', modalTimerId), 5000);
+
+    tabs('.tabheader__item','.tabcontent','.tabheader__items','tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    calc();
+    cards();
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+    timer('.timer', '2022-12-30');
+})
